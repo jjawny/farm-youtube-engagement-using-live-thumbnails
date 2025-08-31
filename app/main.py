88d98e4d2dd307fec6e7b2765a33b83138823955
Johnny <74356539+jjawny@users.noreply.github.com>
2025-09-01 01:09:27 +1000

    added youtube client as a dependency to inject

--- app/main.py
+++ app/main.py
@@ -1,9 +1,31 @@
-from fastapi import FastAPI
-from dotenv import load_dotenv
+from googleapiclient.discovery import build
+from contextlib import asynccontextmanager
+from fastapi import FastAPI, HTTPException
 from app.routes import engagement, ping
+from dotenv import load_dotenv
+import os
 
 load_dotenv()
 
-app = FastAPI()
+
+@asynccontextmanager
+async def lifespan(app: FastAPI):
+    """
+    Exposes shared resources for the lifespan of the web API; like a lightweight DI container.
+    """
+    if not (api_key := os.getenv("YOUTUBE_API_KEY")):
+        raise HTTPException(status_code=500, detail="Missing YOUTUBE_API_KEY")
+
+    app.state.youtube = build("youtube", "v3", developerKey=api_key)
+
+    try:
+        yield
+    finally:
+        # No close function to call, but remove the ref for GC to collect
+        app.state.youtube = None
+
+
+app = FastAPI(lifespan=lifespan)
+
 app.include_router(ping.router)
 app.include_router(engagement.router)
