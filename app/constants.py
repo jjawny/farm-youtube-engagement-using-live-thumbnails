60e1152f1cd50a64a7fd975f95c1470b8573bed3
Johnny <74356539+jjawny@users.noreply.github.com>
2025-09-03 16:51:00 +1000

    update constants

--- app/constants.py
+++ app/constants.py
@@ -15,7 +15,7 @@ BASE_PATH: Path = Path(__file__).resolve().parent  # ./app
 
 # Relative path from the base path
 MOCK_PFP_PATH = BASE_PATH / "assets/mock_pfp.jpeg"
-BASE_THUMBNAIL_PATH = BASE_PATH / "assets/mock_thumbnail.jpeg"
+BASE_THUMBNAIL_PATH = BASE_PATH / "assets/base_thumbnail.jpeg"
 OUTPUT_THUMBNAIL_PATH = (
     BASE_PATH / "../generated_thumbnail"
 )  # the file_name is a parameter
@@ -23,4 +23,3 @@ OUTPUT_THUMBNAIL_PATH = (
 # Pixel sizes
 PFP_SIZE = (214, 214)
 PFP_BORDER_RADIUS = 50
-THUMBNAIL_SIZE = (1280, 720)
